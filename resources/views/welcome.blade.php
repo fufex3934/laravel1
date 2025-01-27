62149f7ea3976d11d7138628c7a74716f5981581
fufex3934 <fufawakgari174@gmail.com>
2025-01-27 11:52:32 +0300

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fufa Network</title>
+</head>
+<body>
+    <h1>Welcome to the Fufa Network</h1>
+    <p>Click the button below to view the list of fufas.</p>
+    <a href="" class="btn">Find Fufas!</a>
+</body>
+</html>
