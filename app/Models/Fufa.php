1a40c4623e1df6905322d35ba9dfb7f20c4f8b2f
fufex3934 <fufawakgari174@gmail.com>
2025-01-28 14:58:11 +0300

    orm

--- app/Models/Fufa.php
+++ app/Models/Fufa.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Fufa extends Model
 {
+    protected $fillable = ['name','skill','bio'];
     /** @use HasFactory<\Database\Factories\FufaFactory> */
     use HasFactory;
 }
