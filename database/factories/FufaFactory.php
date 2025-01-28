29e23836a5414938faf7b32ae1b2c1bc0ba6ec97
fufex3934 <fufawakgari174@gmail.com>
2025-01-28 14:41:06 +0300

    database

---
+++ database/factories/FufaFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Fufa>
+ */
+class FufaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
