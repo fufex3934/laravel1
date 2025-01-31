9f645294ffc619e15683ffb9e8b71e92a2ac8033
fufex3934 <fufawakgari174@gmail.com>
2025-01-31 14:10:22 +0300

    foreign key

---
+++ app/Models/Dojo.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Dojo extends Model
+{
+    /** @use HasFactory<\Database\Factories\DojoFactory> */
+    protected $fillable = ['name','location','description'];
+    use HasFactory;
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $this->call([
+            DojoSeeder::class,
             FufaSeeder::class,
         ]);
     }
