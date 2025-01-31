7b46409ac3914c09619a959574fed104348c91f0
fufex3934 <fufawakgari174@gmail.com>
2025-01-31 15:10:21 +0300

    displaying form errors

--- resources/views/fufas/create.blade.php
+++ resources/views/fufas/create.blade.php
@@ -20,6 +20,7 @@
           type="number" 
           id="skill" 
           name="skill" 
+          value="{{ old('skill') }}"
           required
         >
       
@@ -30,14 +31,14 @@
           id="bio" 
           name="bio" 
           required
-        ></textarea>
+        >{{old('bio')}}</textarea>
       
         <!-- select a dojo -->
         <label for="dojo_id">Dojo:</label>
         <select id="dojo_id" name="dojo_id" required>
           <option value="" disabled selected>Select a dojo</option>
           @foreach($dojos as $dojo)
-          <option value="{{$dojo->id}}">{{$dojo->name}}</option>
+          <option value="{{$dojo->id}}" {{$dojo->id == old('dojo_id')? 'selected':''}}>{{$dojo->name}}</option>
           @endforeach
           
         </select>
@@ -45,6 +46,14 @@
         <button type="submit" class="btn mt-4">Create Fufa</button>
       
         <!-- validation errors -->
+        @if($errors->any())
+        <ul class="px-4 py-2 bg-red-100">
+            @foreach($errors->all() as $error)
+            <li class="my-2 text-red-500">{{ $error }}</li>
+            @endforeach
+        </ul>
+
+        @endif
         
       </form>
 </x-layout>
